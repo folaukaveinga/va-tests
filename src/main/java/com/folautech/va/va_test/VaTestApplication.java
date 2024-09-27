8b8b878f535ba8e51919d3db111a2018981b8ccd
folaukaveinga <folau.kaveinga@va.gov>
2024-09-27 17:53:25 -0600

    setup

---
+++ src/main/java/com/folautech/va/va_test/VaTestApplication.java
@@ -0,0 +1,13 @@
+package com.folautech.va.va_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folautech/va/va_test/VaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.va.va_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
